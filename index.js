9bdb82886c6da02f6157259b9658a73beeab52ae
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-13 18:48:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function produceDrivingRange(blocks) {return function(blockRange) {
+console.log(blockRange)
   return blockRange > blocks ? `${blockRange - blocks} out of range`:`within range by ${blockRange - blocks}`
   }
 }
