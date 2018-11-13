ac5dd0e9c1e3f0c470344e2b55bf0d1fecd46f88
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-13 18:49:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function produceDrivingRange(blocks) {return function(blockRange) {
-console.log(parseInt(blockRange))
+const blocksToIntegers = 
   return blockRange > blocks ? `${blockRange - blocks} out of range`:`within range by ${blockRange - blocks}`
   }
 }
